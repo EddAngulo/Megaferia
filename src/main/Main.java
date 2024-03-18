8aeb18a1fbaf0f884d7d4415f70852fab886099c
edangulo <edangulo@uninorte.edu.co>
2024-03-18 13:33:00 -0500

    First commit

---
+++ src/main/Main.java
@@ -0,0 +1,18 @@
+package main;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author estudiante
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+    
+}
