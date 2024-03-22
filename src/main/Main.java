c26eade188cee13d246845966b48a8af3aec8a2e
edangulo <edangulo@uninorte.edu.co>
2024-03-22 09:49:48 -0500

    Test: Conflicts

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,6 +13,7 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("This is a conflict");
     }
     
 }
